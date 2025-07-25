5f4d30511ed906ff47eedf7885f7e2def5a84333
Libukai <xiaobuyao@gmail.com>
2025-07-25 12:13:18 +0800

    更新 AI 辅助开发文档

--- src/anki_image_editor/__init__.py
+++ src/anki_image_editor/__init__.py
@@ -4,11 +4,11 @@ from anki import version
 try:
     major, minor, patch = version.split(".")[:3]
     version_tuple = (int(major), int(minor), int(patch))
-    
+
     # Require Anki 25.6+ for modern API support
     if version_tuple < (25, 6, 0):
         raise Exception(f"This addon requires Anki 25.6+. Current version: {version}")
-        
+
 except ValueError:
     # Allow development/beta versions to proceed
     pass
